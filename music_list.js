c28d693ad4b9f82a42d21d7a50c612cf3183a794
muthumeenakshi403 <muthumeenakshi403@gmail.com>
2025-01-25 13:40:07 +0530

    initial commit

---
+++ music_list.js
@@ -0,0 +1,34 @@
+let allMusic = [
+    {
+      name: "Speechless",
+      artist: "Naomi Scott",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Look What You Made Me Do",
+      artist: "Taylor Swift",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Wildest Dream",
+      artist: "Taylor Swift",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Firework",
+      artist: "Katty Perry",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Believer",
+      artist: "Imagine Dragons",
+      img: "music-5",
+      src: "music-5"
+    },
+   
+    
+  ];
